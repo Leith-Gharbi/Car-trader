7e4ed5d1a50d2d7e81bd6ad689859f7d9bba2245
Gharbi <leithgharbi@yahoo.fr>
2024-05-09 23:53:53 +0200

    Unique Dynamique Paths

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+      voluptatem, minima nihil delectus necessitatibus facere perferendis eum
+      provident recusandae fugit eveniet dolore adipisci quisquam inventore
+      neque, libero molestias. Facere, distinctio!
+    </p>
+  </div>
+</template>
---
+++ components/Car/Detail/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-10">
+    <img
+      class="w-full"
+      src="https://imtc.qccdn.fr/test/voiture-monospace/zoom/renault-scenic-iii-dci-110-energy-eco2_001.jpg"
+      alt=""
+    />
+    <h1 class="mt-10 text-4xl">SCENIC 3</h1>
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">5 seats</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">67.44 miles</p>
+      </div>
+      <div>
+        <p class="font-bold text-2xl">$ 6500</p>
+      </div>
+    </div>
+  </div>
+</template>
