635db4f176dbe55cc5268b70ccf3b3d836473ddf
Gharbi <leithgharbi@yahoo.fr>
2024-05-09 23:40:25 +0200

    Filling the page with components

---
+++ components/Car/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <div class="flex h-full">
+      <img
+        src="https://imtc.qccdn.fr/test/voiture-monospace/zoom/renault-scenic-iii-dci-110-energy-eco2_001.jpg"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Scenic 2</h1>
+          <p class="text-grey-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            distinctio amet consequatur expedita, aliquid iure. Sunt rerum
+            laudantium aliquid architecto exercitationem? Quaerat iste beatae
+            voluptatum quasi. Laborum dignissimos impedit odit!
+          </p>
+        </div>
+        <h1 class="mt-auto text-xl">$39,555</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <h2>City and make</h2>
+    <NavBar />
+
+    <!-- CARS PAGE -->
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <div class="mt-32 flex">
+        <CarSideBar />
+        <CarCards />
+      </div>
+    </div>
+    <!-- CARS PAGE -->
   </div>
 </template>
