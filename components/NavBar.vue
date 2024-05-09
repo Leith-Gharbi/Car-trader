c762cfc8cb25e8947b213399cdb82bb9725c8502
Gharbi <leithgharbi@yahoo.fr>
2024-05-10 01:05:53 +0200

    The NuxtLink Component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,11 @@
+
+import type { NuxtLink } from '#build/components';
 <template>
   <!-- NAVBAR -->
   <header
     class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
-    <a class="text-3xl font-mono" href="">Car Trader</a>
+    <NuxtLink class="text-3xl font-mono" to="/">Car Trader</NuxtLink>
   </header>
   <!-- NAVBAR -->
 </template>
