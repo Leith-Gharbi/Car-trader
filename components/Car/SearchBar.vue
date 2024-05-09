75f7f4109f2832673f7dad5c026795c9a4a241bf
Gharbi <leithgharbi@yahoo.fr>
2024-05-10 01:21:46 +0200

    Adding Programmatic Navigation

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -3,10 +3,25 @@
     class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
   >
     <input
+      v-model="city"
+      :class="cityError ? 'border-red-500 border' : ''"
       type="text"
       class="py-3 px-5 w-full rounded-full focus:outline-none"
       placeholder="Search by city ..."
     />
-    <button class="bg-sky-500 px-10 text-white">Search</button>
+    <button class="bg-sky-500 px-10 text-white" @click="handelSearch">
+      Search
+    </button>
   </div>
 </template>
+
+<script setup>
+const city = ref("");
+const cityError = ref(false);
+const handelSearch = () => {
+  if (!city.value) {
+   return cityError.value = true;
+  }
+  navigateTo(`/city/${city.value}/car/test`);
+};
+</script>
