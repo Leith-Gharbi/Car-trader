cca7af5d386b2b379578c32071909dceaff7ca50
Gharbi <leithgharbi@yahoo.fr>
2024-05-12 12:32:42 +0200

    Dynamicallly Rendering the Car Details Page

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,25 +1,25 @@
 <template>
   <div
+    @click="navigateTo(`/car/${car.name}-${car.id}`)"
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
     <div class="flex h-full">
-      <img
-        src="https://imtc.qccdn.fr/test/voiture-monospace/zoom/renault-scenic-iii-dci-110-energy-eco2_001.jpg"
-        alt=""
-        class="w-[300px] h-full"
-      />
+      <img :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Scenic 2</h1>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
           <p class="text-grey-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-            distinctio amet consequatur expedita, aliquid iure. Sunt rerum
-            laudantium aliquid architecto exercitationem? Quaerat iste beatae
-            voluptatum quasi. Laborum dignissimos impedit odit!
+            {{ car.description }}
           </p>
         </div>
-        <h1 class="mt-auto text-xl">$39,555</h1>
+        <h1 class="mt-auto text-xl">${{ car.price }}</h1>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
