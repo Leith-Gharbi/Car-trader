b79a8ec38e2cd76fb96ebb60de1a8f24dff95956
Gharbi <leithgharbi@yahoo.fr>
2024-05-12 16:13:38 +0200

    Improving Functionality With Utilities

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -4,7 +4,7 @@
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
     <div class="flex h-full">
-      <img :src="car.url" alt="" class="w-[300px] h-full" />
+      <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
