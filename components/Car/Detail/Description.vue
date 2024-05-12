7935edbe088675171bd6e44b6f09af064538c50c
Gharbi <leithgharbi@yahoo.fr>
2024-05-12 12:53:05 +0200

    Dynamically Rendering the Car Details Page

--- components/Car/Detail/Description.vue
+++ components/Car/Detail/Description.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="mt-5">
     <p class="mb-10">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
-      voluptatem, minima nihil delectus necessitatibus facere perferendis eum
-      provident recusandae fugit eveniet dolore adipisci quisquam inventore
-      neque, libero molestias. Facere, distinctio!
+      {{ description }}
     </p>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  description: String,
+});
+</script>
