bfc93134f4cbcfd3f0a429263a2995b269f21fb3
Gharbi <leithgharbi@yahoo.fr>
2024-05-20 19:13:10 +0200

    Adding the Car Price Filter

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+    <!-- LOCATION START  -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
       <h3 class="text-blue-400 capitalize" @click="updateModal('location')">
@@ -18,31 +19,99 @@
         </button>
       </div>
     </div>
-
+    <!-- LOCATION END  -->
+    <!-- MAKE START  -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+        {{ route.params.make || "Any" }}
+      </h3>
+      <div
+        v-if="modal.make"
+        class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+      >
+        <h4
+          v-for="make in makes"
+          :key="make"
+          class="w-1/3"
+          @click="onChangeMake(make)"
+        >
+          {{ make }}
+        </h4>
+      </div>
     </div>
+    <!-- MAKE END  -->
 
+    <!-- Price START  -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize">Any</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+        {{ priceRangeText }}
+      </h3>
+
+      <div
+        class="absolute border sahow left-56 p-5 top-1 -m-1 bg-white"
+        v-if="modal.price"
+      >
+        <input
+          class="border p-1 rounded"
+          type="number"
+          placeholder="Min"
+          v-model="priceRange.min"
+        />
+        <input
+          class="border p-1 rounded"
+          type="number"
+          placeholder="Max"
+          v-model="priceRange.max"
+        />
+        <button
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+          @click="onChangePrice"
+        >
+          Apply
+        </button>
+      </div>
     </div>
   </div>
+  <!-- Price END -->
 </template>
 
 <script setup>
+const { makes } = useCars();
 const modal = ref({
   make: false,
   location: false,
   price: false,
 });
+const priceRange = ref({
+  min: "",
+  mak: "",
+});
 
 const city = ref("");
 const route = useRoute();
+const router = useRouter();
 
 const updateModal = (key) => {
   modal.value[key] = !modal.value[key];
+};
+
+const onChangeMake = (make) => {
+  updateModal("make");
+  navigateTo(`/city/${route.params.city}/car/${make}`);
+};
+const onChangePrice = () => {
+  updateModal("price");
+  if (priceRange.value.max && priceRange.value.min) {
+    if (priceRange.value.min > priceRange.value.max) return;
+  }
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max,
+    },
+  });
 };
 
 const onChangeLocation = () => {
@@ -57,4 +126,18 @@ const onChangeLocation = () => {
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = "";
 };
+
+const priceRangeText = computed(() => {
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+  if (!minPrice && !maxPrice) {
+    return "Any";
+  } else if (!minPrice && maxPrice) {
+    return `< $${maxPrice}`;
+  } else if (minPrice && !maxPrice) {
+    return `> $${minPrice}`;
+  } else {
+    return `$${minPrice}-$${maxPrice}`;
+  }
+});
 </script>
